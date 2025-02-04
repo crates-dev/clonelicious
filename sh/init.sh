8ef5f90789790ea06af120c1dea91b77537458c3
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:31 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/clonelicious.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/clonelicious.git;
 git remote add github git@github.com:ltpp-universe/clonelicious.git;
 git remote add origin git@github.com:ltpp-universe/clonelicious.git;
